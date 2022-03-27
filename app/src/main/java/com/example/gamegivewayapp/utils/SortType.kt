6805e8365b05da935422813bd45a28451ac36e08
OlegAndroidDev <100316666+OlegAndroidDev@users.noreply.github.com>
2022-03-27 16:35:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gamegivewayapp/utils/SortType.kt
@@ -0,0 +1,18 @@
+package com.example.gamegivewayapp.utils
+
+enum class SortType(val realValue: String) {
+    DATE("date"),
+    VALUE("value"),
+    POPULARITY("popularity")
+}
+
+enum class PlatformType(val realValue: String) {
+    PC("pc"),
+    PS4("ps4"),
+    ANDROID("android")
+}
+
+enum class NewPlatformType(val realValue: String) {
+    PC("pc"),
+    PS4("playstation 4")
+}
